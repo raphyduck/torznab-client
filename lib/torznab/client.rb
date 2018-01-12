372710fa2b6b69163dcfbb94e8c35389f17a95de
Nicolas MERELLI <nicolas.merelli@gmail.com>
2018-01-12 20:18:35 +0000

    feat(client): adding caps extension

--- lib/torznab/client.rb
+++ lib/torznab/client.rb
@@ -1,4 +1,24 @@
+require 'torznab/client/caps'
+
+# Torznab APIs management
 module Torznab
-  module Client # :nodoc:
+  # @see https://github.com/Sonarr/Sonarr/wiki/Implementing-a-Torznab-indexer
+  # @see http://newznab.readthedocs.io/en/latest/misc/api/#caps
+  # Ruby client to Torznab APIs.
+  module Client
+    extend Torznab::Client::Caps
+
+    class << self
+      # Returns a new Torznab::Client instance after initializing it
+      #
+      # @param [String] api_url
+      # @param [String] api_key
+      # @return [Torznab::Client] Initialized Torznab::Client instance
+      # @raise [Torznab::Client::Errors::CapsError]
+      def new(api_url, api_key = nil)
+        fetch_caps_from_url api_url, api_key
+        self
+      end
+    end
   end
 end
--- lib/torznab/client/version.rb
+++ lib/torznab/client/version.rb
@@ -1,5 +1,5 @@
 module Torznab
   module Client
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.1.0'.freeze #:nodoc:
   end
 end
